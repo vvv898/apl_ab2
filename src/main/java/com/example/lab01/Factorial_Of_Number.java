6e15f48044fc8476f63178bcc8d5d3c5ae805228
Вікторія Пекар <viktoriia.pekar@student.karazin.ua>
2025-11-17 14:17:08 +0200

    test 2 change

--- src/main/java/com/example/lab01/Factorial_Of_Number.java
+++ src/main/java/com/example/lab01/Factorial_Of_Number.java
@@ -10,7 +10,7 @@ public class Factorial_Of_Number {
     long factorial_a;
 
     public void inputAndCalculate() {
-        System.out.print("\nEnter New number:");
+        System.out.print("\nNEW NEW:");
         int buff_a = sc.nextInt();
         setA(buff_a);
 
